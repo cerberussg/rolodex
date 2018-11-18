7a62a02f19e5a1229a1161cef35e5e7517d6f555
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-11-18 02:41:35 -0500

    Added db storage for form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,4 +10,24 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        flash[:success] = "Contact added!"
+        format.html { redirect_to root_path }
+      else
+        flash[:danger] = "Contact couldn't be added"
+        format.html {render :new }
+      end
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name,:company,:email,:phone,:address,:group_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   get 'contacts/index', as: 'contacts'
   get 'contacts/new', as: 'new_contact'
+  post '/contacts/create', to: 'contacts#create'
   root 'contacts#index'
 end
