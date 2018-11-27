f1528cbffa6c14bceabbea72c4014c131575e27d
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-11-27 16:29:44 -0500

    Edit Devise default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
 
   get 'dashboard/index'
   get 'home/index'
-  devise_for :users
-  #get 'contacts/index', as: 'contacts'
-  #get 'contacts/new', as: 'new_contact'
-  #post '/contacts/create', as: 'create_contact'
-  #get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
-  #patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
-  #delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   resources :contacts, except: [:show] do
     get 'autocomplete', on: :collection
   end
