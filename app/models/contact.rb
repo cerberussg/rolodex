ba34cc7971abe672d38b5d58b3fa0e449f679cec
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-12-04 13:05:26 -0500

    Modified seeds file to accept user references, updated kaminari view css

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,16 +8,13 @@ class Contact < ApplicationRecord
   validates :name, :email, :group_id, presence: true
   validates :name, length: { minimum: 2 }
 
-
   def gravatar
     hash = Digest::MD5.hexdigest(email.downcase)
 
     "https://www.gravatar.com/avatar/#{hash}"
   end
 
-  scope :search, -> (term) do
-    where('LOWER(name) LIKE :term or LOWER(company) LIKE :term or LOWER(email) LIKE :term', term: "%#{term.downcase}%")if term.present?
-  end
+  scope :search, ->(term) { where('LOWER(name) LIKE :term or LOWER(company) LIKE :term or LOWER(email) LIKE :term', term: "%#{term.downcase}%")if term.present? }
 
-  scope :by_group, -> (group_id) { where(group_id: group_id) if group_id.present? }
+  scope :by_group, ->(group_id) { where(group_id: group_id) if group_id.present? }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,47 @@
+# frozen_string_literal: true
+
 Contact.destroy_all
 Group.destroy_all
+User.destroy_all
 
-group_ids = []
+user_ids = []
+user_ids << User.create(
+  name: 'John Doe',
+  email: 'johndoe@text.com',
+  password: 'secret'
+).id
+user_ids << User.create(
+  name: 'Jane Roe',
+  email: 'janeroe@text.com',
+  password: 'secret'
+).id
 
-group_ids << Group.create(name: "Client").id
-group_ids << Group.create(name: "Family").id
-group_ids << Group.create(name: "Friend").id
+p "#{user_ids.count} Users created"
+
+group_ids = { user_ids[0] => [], user_ids[1] => [] }
+
+group_ids[user_ids[0]] << Group.create(name: 'Client', user_id: user_ids[0]).id
+group_ids[user_ids[0]] << Group.create(name: 'Family', user_id: user_ids[0]).id
+group_ids[user_ids[1]] << Group.create(name: 'Friend', user_id: user_ids[1]).id
+group_ids[user_ids[1]] << Group.create(name: 'Work', user_id: user_ids[1]).id
 
 p "#{group_ids.count} Groups created"
 
 group_count = group_ids.length
-number_of_contacts = 20
+number_of_contacts = 60
 contacts = []
 
 number_of_contacts.times do |i|
+  user_id = user_ids[Random.rand(0...2)]
+
   new_contact = {
     name: Faker::Name.name,
     email: Faker::Internet.email,
     company: Faker::Company.name,
     phone: Faker::PhoneNumber.cell_phone,
     address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
-    group_id: group_ids[Random.rand(0...group_count)]
+    group_id: group_ids[user_id][Random.rand(0...group_count)],
+    user_id: user_id
   }
   contacts.push(new_contact)
 end
